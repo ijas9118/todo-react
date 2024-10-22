f8a39703b00f69b69b7780a02a589f3fb7ec1204
Ijas <ahammedijas.official@gmail.com>
2024-10-22 06:38:51 +0530

    Functionality done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "../assets/todo-logo.svg";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const inputRef = useRef();
+  const [todoList, setTodoList] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id) => {
+    setTodoList((prev) => {
+      return prev.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const toggle = (id) => {
+    setTodoList((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isComplete: !todo.isComplete };
+        }
+        return todo;
+      });
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
+
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       <div className="flex items-center mt-7 gap-2">
@@ -15,18 +59,31 @@ const Todo = () => {
         <input
           type="text"
           placeholder="Add your task"
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+          onClick={add}
+        >
           ADD +
         </button>
       </div>
 
       {/* Todo list */}
       <div>
-        <TodoItems />
-        <TodoItems />
-        <TodoItems />
+        {todoList.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              text={item.text}
+              isComplete={item.isComplete}
+              id={item.id}
+              deleteTodo={deleteTodo}
+              toggle={toggle}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import tick from '../assets/tick.svg';
-import cross from '../assets/cross.svg';
+import noTick from '../assets/no-tick.svg';
 import delete_icon from '../assets/delete.svg';
 
-const TodoItems = () => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-      <div className='flex flex-1 items-center cursor-pointer'>
-        <img src={tick} alt="" className='w-7'/>
-        <p className='text-slate-700 ml-4 text-[17px]'>Learn React JS</p>
+      <div onClick={() => toggle(id)} className='flex flex-1 items-center cursor-pointer'>
+        <img src={isComplete ? tick : noTick} alt="" className='w-6'/>
+        <p className={`text-slate-700 ml-4 text-[17px] ${isComplete ? "line-through" : ""}`}>{text}</p>
       </div>
 
-      <img src={delete_icon} alt="" className='w-6 cursor-pointer'/>
+      <img src={delete_icon} alt="" className='w-5 cursor-pointer' onClick={() => deleteTodo(id)}/>
     </div>
   )
 }
